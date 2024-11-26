3ad7550364bb7bff5cb6afa073b3977d643667de
Stormatar <stormatars@gmail.com>
2024-11-26 12:38:46 -0500

    Apply Your Knowledge

---
+++ apply/scripts/scripts.js
@@ -0,0 +1,13 @@
+/*
+Name: Ronan Norbury
+Date: 11/26/2024
+File: Apply Your Knowledge
+*/
+
+// Function to display content //
+function content() {
+    var text = document.getElementById("new");
+    text.textContent = "Hi. I am a new paragraph that was created with a JavaScript function.";
+    text.style.color = "#c0145f";
+    text.style.fontSize = "2em";
+}
