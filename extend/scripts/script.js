30a3eb17dd42d68dfd0a1085a2bf8ff52eb1c2dc
Stormatar <stormatars@gmail.com>
2024-11-28 19:19:02 -0500

    Extend Your Knowledge

---
+++ extend/scripts/script.js
@@ -0,0 +1,14 @@
+/*
+    Name: Ronan Norbury
+    Date: 11/26/2024
+    File: Apply Your Knowledge
+*/
+
+$(document).ready(function () {
+    $("#hide").click(function () {
+        $("p").hide();
+    });
+    $("#show").click(function () {
+        $("p").show();
+    });
+})
